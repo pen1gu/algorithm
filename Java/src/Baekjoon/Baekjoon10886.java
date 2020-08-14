2df8ab9b416e06aa1079c23db35b299dc4714b9d
HJ <kkkths@naver.com>
2020-08-14 21:29:52 +0900

    2020 08 14

---
+++ Java/src/Baekjoon/Baekjoon10886.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10886 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int ycnt = 0, ncnt = 0;
+        for (int i = 0; i < input; i++) {
+            int input_cute = scanner.nextInt();
+            if (input_cute == 0) ycnt++;
+            else ncnt++;
+        }
+        scanner.close();
+        System.out.println(ncnt > ycnt ? "Junhee is cute!" : "Junhee is not cute!");
+    }
+}
---
+++ Java/src/Baekjoon/Baekjoon2960.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2960 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        scanner.close();
+
+        boolean[] isPrime = new boolean[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= n; j+=i) {
+                if (!isPrime[j]){
+                    continue;
+                }
+                isPrime[j] = false;
+                cnt++;
+                if (cnt == k){
+                    System.out.println(j);
+                    return;
+                }
+            }
+        }
+    }
+}
