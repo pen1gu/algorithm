6620fdd894c50c439ba4d0241c6ce429caa35a8c
HJ <kkkths@naver.com>
2020-08-25 09:14:58 +0900

    2020 08 25

--- Java/src/Baekjoon/Baekjoon1003.java
+++ Java/src/Baekjoon/Baekjoon1003.java
@@ -1,40 +1,42 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon1003 {
+    BufferedReader bf;
+    int zcnt = 0, ocnt = 0;
 
-    static BufferedReader bf;
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    Baekjoon1003() throws Exception {
         bf = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(bf.readLine());
         for (int i = 0; i < tc; i++) {
+            zcnt = 0;
+            ocnt = 0;
             int n = Integer.parseInt(bf.readLine());
-
-            int[][] arr = new int[n + 1][2];
-
-            if (n == 0) {
-                System.out.println("1 0");
-                continue;
-            }
-
-            if (n == 1) {
-                System.out.println("0 1");
-                continue;
-            }
-
-            arr[0][0] = 1;
-            arr[1][1] = 1;
-
-            for (int j = 2; j <= n; j++) {
-                arr[i][0] = arr[j - 1][0] + arr[j - 2][0];
-                arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
-            }
-
-            System.out.println(arr[n][0] + " " + arr[n][1]);
+            fibo(n);
+            System.out.println(zcnt+" "+ocnt);
         }
+        bf.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Baekjoon1003();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    int fibo(int n) {
+        if (n == 0) {
+            zcnt++;
+            return 0;
+        } else if (n == 1) {
+            ocnt++;
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
     }
 }
---
+++ Java/src/Baekjoon/Baekjoon2749.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon2749 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(bufferedReader.readLine());
+        int pisano_num = 1500000;
+        long[] arr = new long[pisano_num];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i < pisano_num && i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000;
+        }
+
+        if (n >= pisano_num) {
+            n %= pisano_num;
+        }
+
+        System.out.println(arr[(int) n]);
+        bufferedReader.close();
+    }
+}
