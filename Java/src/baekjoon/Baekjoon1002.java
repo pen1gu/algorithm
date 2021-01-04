fd477d831c3535d613c4410d408c3d6ad29e4143
윤현준 <kkkths@naver.com>
2021-01-05 05:43:11 +0900

    수정
    utf-8깨짐

--- Java/src/baekjoon/Baekjoon1002.java
+++ Java/src/baekjoon/Baekjoon1002.java
@@ -1,4 +1,22 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon1002 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hello");
+        stringBuilder.append(" world!");
+
+        stringBuilder.delete(0, stringBuilder.length());
+
+        for (int i = 0; i < N; i++) {
+            int user1_x = scanner.nextInt(), user1_y = scanner.nextInt(), user1_r = scanner.nextInt();
+            int user2_x = scanner.nextInt(), user2_y = scanner.nextInt(), user2_r = scanner.nextInt();
+
+        }
+    }
 }
