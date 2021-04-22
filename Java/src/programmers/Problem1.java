0ebc533352535c3bebe5f0eec3924ae9d24623b8
hyunjun yun <kkkths@naver.com>
2021-04-23 08:39:15 +0900

    create

---
+++ Java/src/programmers/Problem1.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        // n = 1 * 2 * 2 * 2 * 2
+        //5000 -> /2 -> 2500 /2 -> 1250 / 2 -> 625 - 1 -> 624 / 2 -> 322 / 2- > 161 - > 1
+
+
+        scanner.close();
+    }
+
+}
