55ba5d053b5aa755ca46d0f29e1029352d721faf
hyunjun yun <kkkths@naver.com>
2021-04-25 10:08:31 +0900

    create

---
+++ python/baekjoon10816.py
@@ -0,0 +1,28 @@
+N = int(input())
+num_arr = sorted(map(int, input().split(' ')))
+M = int(input())
+sg_arr = map(int, input().split(' '))
+
+
+def binary_search(target, data):
+    data.sort()
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return 0
+
+
+result = ''
+for i in sg_arr:
+    result += str(binary_search(i, num_arr)) + " "
+print(result.strip())
