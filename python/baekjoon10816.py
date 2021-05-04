858d5b8c877fa4104134de2111e96fff26caf8a9
hyunjun yun <kkkths@naver.com>
2021-05-04 19:56:53 +0900

    create

--- python/baekjoon10816.py
+++ python/baekjoon10816.py
@@ -1,28 +1,10 @@
-N = int(input())
-num_arr = sorted(map(int, input().split(' ')))
-M = int(input())
-sg_arr = map(int, input().split(' '))
+from collections import Counter
+import sys
 
-
-def binary_search(target, data):
-    data.sort()
-    start = 0
-    end = len(data) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if data[mid] == target:
-            return mid
-        elif data[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    return 0
-
-
-result = ''
-for i in sg_arr:
-    result += str(binary_search(i, num_arr)) + " "
-print(result.strip())
+input = sys.stdin.readline
+n = int(input())
+s = list(map(int, input().split()))
+m = int(input())
+s_ = list(map(int, input().split()))
+s = Counter(s)
+for i in s_: print(s[i], end=" ")
