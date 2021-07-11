183a8d70c4334464e93979d02abb0736d9437313
윤현준 <kkkths@naver.com>
2021-07-11 20:32:58 +0900

    Create Baekjoon2447_2.java

---
+++ Java/src/baekjoon/Baekjoon2447_2.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2447_2 {
+
+//    char[][] arr;
+
+    public void solution() {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+
+        }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Baekjoon2447_2 b = new Baekjoon2447_2();
+        b.solution();
+    }
+
+}
