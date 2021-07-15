72acaf1a527b72b50d2dc16bd45a7e08901b9ebf
윤현준 <kkkths@naver.com>
2021-07-15 20:51:17 +0900

    Update Baekjoon1193.java

--- Java/src/Baekjoon/Baekjoon1193.java
+++ Java/src/Baekjoon/Baekjoon1193.java
@@ -4,16 +4,35 @@ import java.util.Scanner;
 
 public class Baekjoon1193 {
 
-    Baekjoon1193(){
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+    public void solution(int input) {
+        int crossCount = 1; // 위치한 곳의 대각선 개수
+        int prevCountSum = 0; // 현재까지 칸의 누적 수
 
-        for (int i = 0; i < input; i++) {
+        while (true) {
+            if (input <= prevCountSum + crossCount) {
+                if (crossCount % 2 == 1) {
+                    System.out.println((crossCount - (input - prevCountSum - 1) + "/" + (input - prevCountSum)));
+                    break;
+                } else {
+                    System.out.println((crossCount - (input - prevCountSum - 1) + "/" + (input - prevCountSum)));
+                    break;
+                }
+            } else {
+                prevCountSum += crossCount;
+                crossCount++;
+            }
 
         }
     }
 
     public static void main(String[] args) {
-        new Baekjoon1193();
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+
+        Baekjoon1193 b = new Baekjoon1193();
+        b.solution(input);
     }
+
+
 }
