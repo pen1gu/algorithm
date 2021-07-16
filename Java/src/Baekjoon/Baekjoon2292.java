bef33d1578f52ab9cb72e70605b91184cb74627c
윤현준 <kkkths@naver.com>
2021-07-16 20:53:56 +0900

    Update Baekjoon2292.java

--- Java/src/Baekjoon/Baekjoon2292.java
+++ Java/src/Baekjoon/Baekjoon2292.java
@@ -3,22 +3,30 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon2292 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		scanner.close();
-		System.out.println(solution_beehouse(input));
-	}
 
-	public static int solution_beehouse(int n) {
-		if (n == 1)
-			return 1;
-		int result = 1;
-		for (int i = 1; i <= n; i++) {
-			result += (i - 1) * 6;
-			if (n <= result)
-				break;
-		}
-		return result;
-	}
+    public int solution(int n) {
+        if (n == 1)
+            return 1;
+
+        int result = 1;
+        int floorCount = 0;
+
+        for (int i = 0; i < n; i++) { // 1 6 12 18 24, 1 7 19 37 61
+            result += i * 6;
+            floorCount++;
+
+            if (n <= result)
+                break;
+        }
+        return floorCount;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+
+        Baekjoon2292 b = new Baekjoon2292();
+        System.out.println(b.solution(input));
+    }
 }
