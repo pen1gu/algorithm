de914aedc26b9d2d358dfffcfd970b546db6aca8
윤현준 <kkkths@naver.com>
2021-07-29 02:07:52 +0900

    Update Baekjoon1874.java

--- Java/src/Baekjoon/Baekjoon1874.java
+++ Java/src/Baekjoon/Baekjoon1874.java
@@ -2,6 +2,8 @@ package baekjoon;
 
 public class Baekjoon1874 {
     public static void main(String[] args) {
-
+        String test = "abcde";
+        System.out.println(test.charAt(0));
+        System.out.println(test.substring(test.length() - 1, test.length()));
     }
 }
