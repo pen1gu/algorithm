3e08e0e6985b721a1f95775fa6985a17b26883c6
hyunjun yun <kkkths@naver.com>
2021-08-16 23:24:02 +0900

    delete

--- python/Baekjoon1128.py
+++ python/Baekjoon1128.py
@@ -7,15 +7,11 @@ numArr = map(int, input().split(" "))
 selectedCount = int(input())
 selectedArr = map(int, input().split(" "))
 
-
-for i in numArr:
-    count = 0 
+for i in numArr: 
+    count = 0
     for j in selectedArr:
         if numArr[i] == selectedArr[j]:
             numArr.pop(i)
             count += 1
-<<<<<<< HEAD
     print(count)
-=======
-    print(count)
->>>>>>> b814fea120410ae197a5f421813617e20ab25c0c
+print(count)
